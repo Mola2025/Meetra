5113bc177ff265b07d064916c40fe6f620975f9c
David Molano <davidm0l4n0@gmail.com>
2026-05-21 15:26:20 -0400

    Auth Module Frotend Design:
    - Create login and register form
    - AppRoutes.jsx created to handle path to always open first login
    - Change form mode between login and register
    
    TODO: Ready to implement backend routes and correct registration and login
    - Modify colors of entire page (instead of blue other)

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+// import Navbar from "../components/layouts/NavBar";
+
+import Home from "../features/home/Home";
+import Auth from "../features/auth/Auth";
+
+
+const AppRoutes = () => {
+  return (
+    <>
+      {/* <Navbar /> */}
+      {/* <main style={{ paddingTop: "64px" }}> */}
+      <Routes>
+        <Route path="/" element={<Auth mode="login"/>} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+      {/* </main> */}
+    </>
+  );
+};
+
+export default AppRoutes;

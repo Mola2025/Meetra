55b565e1a4ce0865e4b6d478b1324de66cf6161e
David Molano <davidm0l4n0@gmail.com>
2026-05-25 10:21:28 -0400

    Auth Feature Done / Including Profile Dashboard in this branch:
    - Fixing UI problems from saturday in Profile - Packages missing
    - npm install tailwindcss for /client and root folder
    - Modify vite.cofig to handle tailwindcss in all project
    - Modify Auth.css to dont apply all style to root
    - Profile.jsx and css created and working
    - Profile ready to handle API Calls
    - App Routes Modified to connect Profile route
    
    - TODO: Connect API Calls for auth and Profile when they are done

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/navbar/NavBar";
 
 import Home from "../features/home/Home";
 import Auth from "../features/auth/Auth";
+import Profile from "../features/profile/Profile";
 
 
 const AppRoutes = () => {
@@ -13,6 +14,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Auth mode="login"/>} />
         <Route path="/home" element={<Home/>} />
+        <Route path="/profile" element={<Profile/>}/>
       </Routes>
       </main>
     </>

2328894cde8dd5c1de9b2609aff9686277ac4c45
Mourad Sehboub <139013207+MouradWPF@users.noreply.github.com>
2026-06-20 16:13:08 -0400

    fix: resolve Vite/Tailwind compat issue, fix App.jsx imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import Meeting from "./pages/Meeting";
-import "./App.css";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/meeting" element={<Meeting />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <AppRoutes />;
 }
 
-export default App; 
+export default App;
